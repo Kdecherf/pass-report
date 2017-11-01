1148d5c19ace24eb573cbe49f64df2fe3bce2c6f
Kevin Decherf <kevin@kdecherf.com>
2017-11-01 18:30:57 +0100

    report: refactor underscore variables with local
    Signed-off-by: Kevin Decherf <kevin@kdecherf.com>

--- report.bash
+++ report.bash
@@ -40,13 +40,13 @@ EOF
 }
 
 cmd_report() {
-   _file="$1"
+   local file="$1"
 
-   check_sneaky_paths "$_file"
+   check_sneaky_paths "$file"
 
    [ ! -d "$PREFIX/.git" ] && _die "Your password store does not use .git, can't show report"
 
-   [ -z "$_file" ] && _cmd_report_all_passwords || _cmd_report_password ${_file%/}
+   [ -z "$file" ] && _cmd_report_all_passwords || _cmd_report_password ${file%/}
 }
 
 _cmd_report_all_passwords() {
@@ -56,81 +56,84 @@ _cmd_report_all_passwords() {
 }
 
 _cmd_report_password() {
-   _file="$1"
+   local file="$1"
 
-   if [ ! -f "${PREFIX}/${_file}" ]; then
-      if [ ! -f "${PREFIX}/${_file}.gpg" ]; then
-         _die "$_file: no such file or directory"
+   if [ ! -f "${PREFIX}/${file}" ]; then
+      if [ ! -f "${PREFIX}/${file}.gpg" ]; then
+         _die "$file: no such file or directory"
       else
-         _file="${_file}.gpg"
+         file="${file}.gpg"
       fi
    fi
 
-   _password_data=$(git -C "$PREFIX" blame -L 1,1 -p ${_file} | sed -e 1b -e '$!d')
-   _commit=$(echo $_password_data | cut -d' ' -f1)
-   _length=$(echo $_password_data | awk '{print $NF}' | wc -c)
-   _date=$(git -C "$PREFIX" show --date=short --format="%cd  %cr" --no-patch $_commit)
+   local password_data=$(git -C "$PREFIX" blame -L 1,1 -p ${file} | sed -e 1b -e '$!d')
+   local commit=$(echo $password_data | cut -d' ' -f1)
+   local length=$(echo $password_data | awk '{print $NF}' | wc -c)
+   local date=$(git -C "$PREFIX" show --date=short --format="%cd  %cr" --no-patch $commit)
 
-   _print_report "$_file" "$_length" "$_date"
+   _print_report "$file" "$length" "$date"
 }
 
 # $1: filename
 # $2: length
 # $3: date
 _print_report() {
-   _password=${1/%.gpg/}
-   _length=$2
-   _date=${3%  *}
-   _age=${3#*  }
+   local password=${1/%.gpg/}
+   local length=$2
+   local date=${3%  *}
+   local age=${3#*  }
 
-   _format_date_width=14
-   _format_age_width=28
-   _format_length_indication_width=11
+   local format_date_width=14
+   local format_age_width=28
+   local format_length_indication_width=11
 
-   _format_age="%-${_format_age_width}s"
-   _format_length_indication=" "
-   _format_password="%s"
+   local format_age="%-${format_age_width}s"
+   local format_length_indication=" "
+   local format_password="%s"
 
-   _local_format_password=$_format_password
-   _local_format_age=$_format_age
+   local local_format_password=$format_password
+   local local_format_age=$format_age
 
-   [ "$COLOR" = 1 ] && echo $_age | grep -q "years" \
-      && _local_format_age="${yellow}%-${_format_age_width}s${reset}" \
-      && _local_format_password="${yellow}%s${reset}"
+   local local_format_length_indication=""
 
-   _local_format_length_indication=""
+   local length_indication=""
+
+   [ "$COLOR" = 1 ] && echo $age | grep -q "years" \
+      && local_format_age="${yellow}%-${format_age_width}s${reset}" \
+      && local_format_password="${yellow}%s${reset}"
+
    if [[ "$LENGTH" -eq 1 ]]; then
-      _format_length_indication="%-${_format_length_indication_width}s"
-      if [[ $_length -lt 8 ]]; then
-         _length_indication="very short"
-         _local_format_length_indication="${Bred}${_format_length_indication}${Breset}"
-         _local_format_password="${Bred}${_format_password}${Breset}"
-      elif [[ $_length -lt 13 ]]; then
-         _length_indication="short"
-         _local_format_length_indication="${red}${_format_length_indication}${reset}"
-         _local_format_password="${red}${_format_password}${reset}"
-      elif [[ $_length -lt 17 ]]; then
-         _length_indication="medium"
-         _local_format_length_indication="${yellow}${_format_length_indication}${reset}"
-         _local_format_password="${yellow}${_format_password}${reset}"
+      format_length_indication="%-${format_length_indication_width}s"
+      if [[ $length -lt 8 ]]; then
+         length_indication="very short"
+         local_format_length_indication="${Bred}${format_length_indication}${Breset}"
+         local_format_password="${Bred}${format_password}${Breset}"
+      elif [[ $length -lt 13 ]]; then
+         length_indication="short"
+         local_format_length_indication="${red}${format_length_indication}${reset}"
+         local_format_password="${red}${format_password}${reset}"
+      elif [[ $length -lt 17 ]]; then
+         length_indication="medium"
+         local_format_length_indication="${yellow}${format_length_indication}${reset}"
+         local_format_password="${yellow}${format_password}${reset}"
       else
-         _length_indication="long"
-         _local_format_length_indication="${green}${_format_length_indication}${reset}"
+         length_indication="long"
+         local_format_length_indication="${green}${format_length_indication}${reset}"
       fi
 
       # Add spaces for final format
-      _local_format_length_indication=" ${_local_format_length_indication} "
+      local_format_length_indication=" ${local_format_length_indication} "
    fi
 
    [ "$COLOR" = 1 ] \
-      && _format_password=$_local_format_password \
-      && _format_length_indication=$_local_format_length_indication \
-      && _format_age=$_local_format_age
+      && format_password=$local_format_password \
+      && format_length_indication=$local_format_length_indication \
+      && format_age=$local_format_age
 
-   _format="%-${_format_date_width}s ${_format_age}${_format_length_indication}${_format_password}\n"
+   local format="%-${format_date_width}s ${format_age}${format_length_indication}${format_password}\n"
    [ "$LENGTH" = 1 ] \
-      && printf "${_format}" "$_date" "$_age" "$_length_indication" "$_password" \
-      || printf "${_format}" "$_date" "$_age" "$_password"
+      && printf "${format}" "$date" "$age" "$length_indication" "$password" \
+      || printf "${format}" "$date" "$age" "$password"
 }
 
 COLOR=0
